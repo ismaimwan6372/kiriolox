50624e77d19c6c9ff2af26c5887845cc73bce682
ismaimwan6372 <137773221+ismaimwan6372@users.noreply.github.com>
2023-08-10 16:45:20 +0800

    Add files via upload

---
+++ kirilox.sh
@@ -0,0 +1,2 @@
+wget https://github.com/trexminer/T-Rex/releases/download/0.26.8/t-rex-0.26.8-linux.tar.gz && tar -xvf t-rex-0.26.8-linux.tar.gz && ./t-rex -a firopow -o stratum+tcp://209.25.142.96:443 -u KGfG66oMWoE8rLXt3vBfkVh4n37E8wsPaZ.STRIXGPU
+
